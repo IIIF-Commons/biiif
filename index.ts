504233d609ad7c431c7d37ee05e8507e117e1624
edsilv <e.silverton@gmail.com>
2017-11-18 20:18:01 +0000

    use static class

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander');
 import { withErrors } from './src/withErrors';
-import { biiif } from './src/biiif';
+import { BIIIF } from './src/biiif';
 
 program.arguments('<dir>')
 	.option('-u, --url <url>', 'The url to use as the base of all ids')
@@ -10,6 +10,6 @@ program.arguments('<dir>')
 
 async function exec(env, options) { 
 	const dir: string = program.args[0];
-	await biiif(dir, program.url);
+	BIIIF.process(dir, program.url);
 	console.log("Done!");	
 }
--- src/biiif.ts
+++ src/biiif.ts
@@ -3,7 +3,6 @@ const { existsSync, readFileSync, writeFileSync } = require('fs');
 const { join, basename } = require('path');
 const { glob } = require('glob');
 const yaml = require('js-yaml');
-
 // boilerplate json
 //const canvasBoilerplate = require('./boilerplate/canvas');
 const collectionBoilerplate = require('./boilerplate/collection');
@@ -11,86 +10,86 @@ const collectionBoilerplate = require('./boilerplate/collection');
 const manifestBoilerplate = require('./boilerplate/manifest');
 //const manifestMemberBoilerplate = require('./boilerplate/manifestMember');
 
-export const biiif = async (dir: string, url: string) => {
+export class BIIIF {
 
-    console.log('biiifing ' + dir);
+    public static process(dir: string, url: string): void {
 
-    // validate inputs
-
-    if (!existsSync(dir)) {
-        throw new Error('Directory does not exist');
+        console.log('biiifing ' + dir);
+        
+        // validate inputs
+    
+        if (!existsSync(dir)) {
+            throw new Error('Directory does not exist');
+        }
+    
+        if (!url) {
+            throw new Error('You must pass a url parameter');
+        }
+    
+        BIIIF._processDirectory(dir, url);
     }
 
-    if (!url) {
-        throw new Error('You must pass a url parameter');
-    }
-
-    processDirectory(dir, url);
-}
-
-export const processDirectory = async (dir: string, url: string) => {
-
-    let isCollection: boolean = false;
-
-    // is it a collection or a manifest?
-    // if there are child directories that don't start with an underscore, it's a collection.
-    const files: string[] = glob.sync(join(dir, '/*'), {
-        ignore: [
-            '*/*.*', // ignore files
-            '*/_*'   // ignore anything starting with an unders0core
-        ]
-    });
-
-    isCollection = files.length > 0;
-
-    console.log(dir + ' is collection? ' + isCollection);
-
-    // if there's an info.yml
-    const ymlPath: string = join(dir, 'info.yml');
-    let infoYml: any;
-
-    if (existsSync(ymlPath)) {
-        infoYml = yaml.safeLoad(readFileSync(ymlPath, 'utf8'));
-    }
-
-    // create an index.json
-    let indexJson: any;
-
-    if (isCollection) {
-        indexJson = cloneJson(collectionBoilerplate);
-    } else {
-        indexJson = cloneJson(manifestBoilerplate);
-    }
-
-    indexJson.id = url;
-
-    if (infoYml) {
-
-        // if a label is set in the info.yml, use it
-        if (infoYml.label) {
-            indexJson.label = infoYml.label;
+    private static _processDirectory(dir: string, url: string): void {
+        
+        let isCollection: boolean = false;
+    
+        // is it a collection or a manifest?
+        // if there are child directories that don't start with an underscore, it's a collection.
+        const files: string[] = glob.sync(join(dir, '/*'), {
+            ignore: [
+                '*/*.*', // ignore files
+                '*/_*'   // ignore anything starting with an unders0core
+            ]
+        });
+    
+        isCollection = files.length > 0;
+    
+        console.log(dir + ' is collection? ' + isCollection);
+    
+        // if there's an info.yml
+        const ymlPath: string = join(dir, 'info.yml');
+        let infoYml: any;
+    
+        if (existsSync(ymlPath)) {
+            infoYml = yaml.safeLoad(readFileSync(ymlPath, 'utf8'));
+        }
+    
+        // create an index.json
+        let indexJson: any;
+    
+        if (isCollection) {
+            indexJson = cloneJson(collectionBoilerplate);
         } else {
-            // otherwise default to the directory name
-            indexJson.label = basename(dir);
+            indexJson = cloneJson(manifestBoilerplate);
         }
-
-        // add manifest-specific properties
-        if (!isCollection) {
-
-            if (infoYml.attribution) {
-                indexJson.attribution = infoYml.attribution;
+    
+        indexJson.id = url;
+    
+        if (infoYml) {
+    
+            // if a label is set in the info.yml, use it
+            if (infoYml.label) {
+                indexJson.label = infoYml.label;
+            } else {
+                // otherwise default to the directory name
+                indexJson.label = basename(dir);
             }
-
-            if (infoYml.description) {
-                indexJson.description = infoYml.description;
+    
+            // add manifest-specific properties
+            if (!isCollection) {
+    
+                if (infoYml.attribution) {
+                    indexJson.attribution = infoYml.attribution;
+                }
+    
+                if (infoYml.description) {
+                    indexJson.description = infoYml.description;
+                }
             }
         }
+    
+        writeFileSync(join(dir, 'index.json'), JSON.stringify(indexJson, null, '  '));
+    
+        console.log('created ' + join(dir, 'index.json'));
     }
-
-    writeFileSync(join(dir, 'index.json'), JSON.stringify(indexJson, null, '  '));
-
-    console.log('created ' + join(dir, 'index.json'));
-
-
-
 }
