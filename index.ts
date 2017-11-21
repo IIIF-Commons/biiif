cc37adda72bf8516c27d75c5f23e4eaa39379c3a
edsilv <e.silverton@gmail.com>
2017-11-21 10:00:42 +0000

    export

--- index.ts
+++ index.ts
@@ -2,24 +2,23 @@ import { Directory } from './Directory';
 const chalk = require('chalk');
 const { existsSync } = require('fs');
 
-export default class BIIIF {
+const biiif = (dir: string, url: string) => {
 
-    public static process(dir: string, url: string): void {
+    console.log(chalk.white('biiifing ' + dir));
+    
+    // validate inputs
 
-        console.log(chalk.white('biiifing ' + dir));
-        
-        // validate inputs
-    
-        if (!existsSync(dir)) {
-            throw new Error('Directory does not exist');
-        }
-    
-        if (!url) {
-            throw new Error('You must pass a url parameter');
-        }
-    
-        new Directory(dir, url);
-
-        console.log("Done!");
+    if (!existsSync(dir)) {
+        throw new Error('Directory does not exist');
     }
+
+    if (!url) {
+        throw new Error('You must pass a url parameter');
+    }
+
+    new Directory(dir, url);
+
+    console.log("Done!");
 }
+
+module.exports = biiif;
