37c0a23c35320f55d89e0015ba4ccdf7db69929b
edsilv <e.silverton@gmail.com>
2017-12-05 10:21:45 +0000

    test log

--- test/test.js
+++ test/test.js
@@ -168,6 +168,7 @@ describe('top collection', async () => {
 
     it('has the correct collection thumbnail id', async () => {
         const id = collectionUrl + '/thumb.png';
+        console.log(thumbnailJson.id);
         assert(thumbnailJson.id === id);
     });
 
