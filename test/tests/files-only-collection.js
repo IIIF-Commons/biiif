59f3076a07b072bdcac7ae5270bb87c8cdd0d5a3
edsilv <e.silverton@gmail.com>
2018-06-11 14:00:27 +0100

    files-only-collection passes

--- test/tests/files-only-collection.js
+++ test/tests/files-only-collection.js
@@ -2,8 +2,6 @@ const common = require("../common");
 const assert = common.assert;
 const basename = common.basename;
 const build = common.build;
-const fs = common.fs;
-const jsonfile = common.jsonfile;
 const mock = common.mock;
 const URL = common.URL;
 const urljoin = common.urljoin;
@@ -14,14 +12,14 @@ const collection = '/files-only-collection';
 const collectionUrl = 'http://test.com/files-only-collection';
 
 it('can build collection', async () => {
-    assert(fs.existsSync(collection));
+    assert(await Utils.fileExists(collection));
     return build(collection, collectionUrl, false);
 }).timeout(1000); // should take less than a second
 
 it('can find collection index.json', async () => {
     const file = '/files-only-collection/index.json';
-    assert(fs.existsSync(file));
-    collectionJson = jsonfile.readFileSync(file);
+    assert(await Utils.fileExists(file));
+    collectionJson = await Utils.readJson(file);
 });
 
 it('has correct collection id', async () => {
@@ -30,8 +28,8 @@ it('has correct collection id', async () => {
 
 it('can find manifest index.json', async () => {
     const file = '/files-only-collection/files-only-manifest/index.json';
-    assert(fs.existsSync(file));
-    manifestJson = jsonfile.readFileSync(file);
+    assert(await Utils.fileExists(file));
+    manifestJson = await Utils.readJson(file);
 });
 
 it('has correct manifest id', async () => {
--- test/top.js
+++ test/top.js
@@ -189,7 +189,7 @@ importTest('utils', './tests/utils');
 importTest('do-promises-work', './tests/do-promises-work');
 importTest('files-only-manifest', './tests/files-only-manifest');
 importTest('files-only-manifest-dat', './tests/files-only-manifest-dat');
-// importTest('files-only-collection', './tests/files-only-collection');
+importTest('files-only-collection', './tests/files-only-collection');
 // importTest('gh-pages', './tests/gh-pages');
 // importTest('collection-no-manifests', './tests/collection-no-manifests');
 // importTest('collection', './tests/collection');
